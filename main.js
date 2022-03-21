c5e1433b509b7905fbd860f5531628c462ce14fb
Keith Tan <keithtanzihao@gmail.com>
2022-03-21 14:07:42 +0800

    express main: Added user CRUD, minor changes to technique post request

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const mongodb = require("mongodb");
 const mongoUtil = require("./mongoUtil");
 const { ObjectID } = require("bson");
 
-const PORT_NUMBER = 3000;
+const PORT_NUMBER = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
@@ -16,11 +16,19 @@ app.use(cors());
 
 const ARTICLES = "articles";
 const TECHNIQUES = "techniques"
+const USERS = "users"
+const REVIEWS = "reviews"
 const mongoURL = process.env.MONGO_URL;
 
 async function main() {
   await mongoUtil.connectDB(process.env.MONGO_URL, "LetsTalk");
 
+  // ---------------------- SANITIZING ---------------------- 
+  app.get("/", function(req, res) {
+    res.send("SERVER IS RUNNING");
+  })
+
+  //  ---------------------- Articles ---------------------- 
   app.post("/articles", async function (req, res) {
     try {
       let { 
@@ -74,7 +82,6 @@ async function main() {
     }
   });
 
-  //  ---------------------- Articles ---------------------- 
   app.get("/articles", async function (req, res) {
     try {
       const db = mongoUtil.getDB();
@@ -147,22 +154,9 @@ async function main() {
   })
 
   //  ---------------------- Techniques ---------------------- 
-  app.get("/techniques", async function(req, res) {
-    try {
-      const db = mongoUtil.getDB();
-      let techniqueArrays = await db.collection(TECHNIQUES).find().toArray();
-      res.json({
-        articles: techniqueArrays,
-      });
-    } catch(error) {
-      console.log(error);
-    }
-  })
-
   app.post("/techniques", async function(req, res) {
     try {
-      let db = mongoUtil.getDB();
-      await db.collection(TECHNIQUES).insertOne({
+      await mongoUtil.getDB().collection(TECHNIQUES).insertOne({
         ...req.body
       })
       res.status(200);
@@ -175,11 +169,21 @@ async function main() {
     }
   })
 
+  app.get("/techniques", async function(req, res) {
+    try {
+      let techniqueArrays = await mongoUtil.getDB().collection(TECHNIQUES).find().toArray();
+      res.json({
+        articles: techniqueArrays,
+      });
+    } catch(error) {
+      console.log(error);
+    }
+  })
+
   app.put("/techniques/:id", async function(req, res) {
     try {
-      let db = mongoUtil.getDB();
-      await db.collection(TECHNIQUES).updateOne({
-        "_id": ObjectId(req.params.id)
+      await mongoUtil.getDB().collection(TECHNIQUES).updateOne({
+        "_id": mongodb.ObjectId(req.params.id)
       }, {
         "$set": {
           "benefits": req.body.benefits,
@@ -198,9 +202,92 @@ async function main() {
   })
 
   app.delete("/techniques/:id", async function(req, res) {
-    
+    try {
+      await mongoUtil.getDB().collection(TECHNIQUES).deleteOne({
+        "_id": mongodb.ObjectId(req.params.id)
+      })
+      res.status(200);
+      res.json({
+        'message': `This is the deleted body: ${req.body}`
+      })
+    } catch(error) {
+      console.log(error);
+    }
   })
 
+  //  ---------------------- USERS ---------------------- 
+  app.post("/users", async function(req, res) {
+    try {
+      await mongoUtil.getDB().collection(USERS).insertOne({
+        ...req.body
+      })
+      res.status(200);
+      res.json({
+        'ADDED NEW USER': req.body
+      })
+    } catch(error) {
+      console.log(error);
+    }
+  })
+  
+  app.get("/users", async function(req, res) {
+    try {
+      let userArray = await mongoUtil.getDB().collection(USERS).find().toArray();
+      res.json({
+        "users": userArray
+      })
+    } catch(error) {
+      console.log(error);
+    }
+  })
+
+  app.put("/users/:id", async function(req, res) {
+    try {
+      let {
+        first_name,
+        last_name,
+        email,
+        username,
+        password,
+        profile_image
+
+      } = req.body;
+
+      await mongoUtil.getDB().collection(USERS).updateOne({
+        "_id": mongodb.ObjectId(req.params.id)
+      }, {
+        "$set": {
+          "first_name": first_name,
+          "last_name": last_name,
+          "email": email,
+          "username": username,
+          "password": password,
+          "profile_image": profile_image
+        }
+      }) 
+      res.status(200);
+      res.json({
+        "NEW USER": `This is the NEW USER ${req.body}`
+      })
+
+    } catch(error) {
+      console.log(error);
+    }
+  })
+
+  app.delete("/users/:id", async function(req, res) {
+    try {
+      await mongoUtil.getDB().collection(USERS).deleteOne({
+        "_id": mongodb.ObjectId(req.params.id)
+      })
+      res.status(200);
+      res.json({
+        'USER DELETE': `This is the deleted body: ${req.body}`
+      })
+    } catch(error) {
+      console.log(error);
+    }
+  })
 }
 
 main();
