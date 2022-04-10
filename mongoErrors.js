1c4fee039e4a7d682ef591d536a2f1d09ebfc61f
Keith Tan <keithtanzihao@gmail.com>
2022-04-10 16:23:21 +0800

    mongoAsyncError, mongoErrors: Error catching helpers

---
+++ mongoErrors.js
@@ -0,0 +1,9 @@
+class mongoErrors extends Error {
+  constructor(message, statusCode) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = mongoErrors
